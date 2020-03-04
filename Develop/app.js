cb7fe9dd31389c260a0b7937f4b0df6e22478dba
Hugo Harley <hpierrel@gmail.com>
2020-03-03 23:34:40 -0500

    new app code

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,307 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function start() {
+    function createEmployee() {
+        let questions = ([
+            {
+                type: "input",
+                message: "Provide team member name:",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Please provide team member ID:",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Please provide team member Email:",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Is the team member an engineer or an intern?",
+                name: "role"
+            },
+        ])
+        inquirer.prompt(questions).then((answers) => {
+            if (answers.role === "engineer") {
+                createEng();
+            }
+            else if (answers.role === "intern") {
+                createIntern();
+            }
+            //prompt and ask is team member engineer or emplyee?
+            //call createEng() or createIntern()
+        })
+    }
+    function createEng() {
+        let engi = ([
+            {
+                type: "input",
+                message: "Please provide your Git Hub username:",
+                name: "github"
+            }
+        ]);
+        inquirer.prompt(engi)
+            .then(const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github, answers.role)
+        createEmployee()
+            ;
+            );
+        // get engineer detils
+        //then create emplyoee
+        // add team member to array
+    }
+    function createIntern() {
+        let intern = ([
+            {
+                type: "input",
+                message: "please provide school:",
+                name: "school"
+            }
+        ]);
+        inquirer.prompt(intern).then(createEmployee());
+    }
+}
+start();
+/*
+let questions = ([
+    {
+        type: "input",
+        message: "Provide team member name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Please provide team member ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Please provide team member Email:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Is the team member the manager, an engineer or an intern?",
+        name: "role"
+    },
+    // {
+    //     type: "input",
+    //     message: "Please provide your Git Hub username",
+    //     name: "github"
+    // },
+    // {
+    //     type: "input",
+    //     message: "please provide school",
+    //     name: "school"
+    // },
+    // {
+    //     type: "input",
+    //     message: "please provide office number",
+    //     name: "officeNumber"
+    // }
+])
+inquirer.prompt(questions).then(answers => {
+    if (answers.role === "engineer") {
+        let engi = ([
+            {
+                type: "input",
+                message: "Please provide your Git Hub username",
+                name: "github"
+            }
+        ]);
+        inquirer.prompt(engi);
+    }
+})
+/*
+async function getManInfo() {
+    try {
+        const manInfo = await inquirer.prompt([
+            {
+                type: "input",
+                message: "What is Managers's name?",
+                name: "manName"
+            },
+            {
+                type: "input",
+                message: "What is Managers's ID?",
+                name: "manId"
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+            },
+            {
+                type: "input",
+                message: "Please enter Manager's email address:",
+                name: "manEmail"
+            },
+            {
+                type: "input",
+                message: "Please enter Manager's office number:",
+                name: "manOfficeNumber"
+            }
+        ]);
+        let response = manInfo;
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getTeamInfo = async () => {
+
+    const addNewMember = [
+        {
+            type: "confirm",
+            message: "Add a team member?",
+            name: "newmember"
+        }
+    ];
+
+    const teamType = [
+        {
+            type: "list",
+            message: "Which team member type is being added?",
+            choices: ["Engineer", "Intern"],
+            name: "memberType"
+        }
+    ];
+
+    const allTeaminfo = [
+        {
+            type: "input",
+            message: "Team Member's name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Team Member ID:",
+            name: "ID"
+        },
+        {
+            type: "input",
+            message: "Team member's email:",
+            name: "email"
+        }
+    ];
+
+    const getGithub = [
+        {
+            type: "input",
+            message: "Engineer's Github:",
+            name: "github"
+        }
+    ];
+
+    const getSchool = [
+        {
+            type: "input",
+            message: "Intern's school: ",
+            name: "school"
+        }
+    ];
+
+
+    let newmember = true;
+
+    let members = [""];
+
+    while (newmember) {
+
+        let res = await inquirer.prompt(addNewMember);
+
+        if (res.newmember) {
+
+            res = await inquirer.prompt(teamType);
+
+            if (res.teamType == "Engineer") {
+                console.log("Engineer!");
+
+                let teamInfo = await inquirer.prompt(allTeaminfo);
+
+                let githubUsername = await inquirer.prompt(getGithub);
+
+                members.push({ name: teamInfo.name, email: teamInfo.email, guser: githubUsername.github, role: "Engineer" });
+            }
+            else if (res.teamType == "Intern") {
+                console.log("Intern!");
+
+                let teamInfo = await inquirer.prompt(allTeaminfo);
+
+                let school = await inquirer.prompt(getSchool);
+
+                members.push({ name: teamInfo.name, email: teamInfo.email, school: school.school, role: "Intern" });
+            }
+        }
+        else {
+            newmember = false;
+        }
+
+
+    }
+
+    console.log(members);
+
+}
+
+async function init() {
+
+    let manInfo = await getManInfo();
+    let teamInfo = await getTeamInfo();
+
+
+}
+
+
+
+
+
+init();
+/*
+getTeamInfo();
+
+async function addNewMember() {
+    try {
+        const newmember = await inquirer.prompt({
+            type: "confirm",
+            message: "Add new team member?",
+            name: "newmember"
+        });
+        try {
+            const teamType = await inquirer.prompt({
+                type: "list",
+                message: "Which team member type is being added?",
+                choices: ["Engineer", "Intern"],
+                name: "memberType"
+            });
+            try {
+                const allTeamInfo = await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Team Member's Name:",
+                        name: "name"
+                    },
+                    {
+                        type: "input",
+                        message: "Team Member ID?",
+                        name: "ID"
+                    },
+                    {
+                        type: "input",
+                        message: "Team member's email:",
+                        name: "email"
+                    }
+                ])
+            }
+
+
+        }
+        }
+
+
+    }
+}
+
 inquirer
     .prompt([
         {
@@ -34,7 +332,22 @@ inquirer
             type: "input",
             message: "What is employee's role?",
             name: "role"
-        } //should i just ask all the questions here? 
+        },
+        {
+            type: "input",
+            message: "What is employee's school?",
+            name: "school"
+        },
+        {
+            type: "input",
+            message: "What is employee's github?",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "What is employee's office Number?",
+            name: "officeNumber"
+        }
     ])
     .then(function (response) {
         //should i put the renderer code here?
@@ -44,7 +357,14 @@ inquirer
         else {
             console.log("You forgot your password already?!");
         }
-    });
+    });*/
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -65,3 +385,8 @@ inquirer
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
+
+const manager = new Manager(name, ID, email, officenumber);
+const engineer = new Engineer(answers.name, answers.ID, answers.email, answers.github);
+const intern = new Intern(answers.name, answers.ID, answers.email, answers.school);
